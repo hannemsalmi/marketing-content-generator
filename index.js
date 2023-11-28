f947d0922f3bbf3a16a42428c1192ef24dfd54df
JaakkoRitvas <jaakko.ritvas@metropolia.fi>
2023-11-28 06:32:17 +0200

    Siirto

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const app = express();
 const port = process.env.PORT || 3005;
-const generateSearchQuery = require('./controllers/searchQuery');
+const generateSearchQuery = require('./searchQuery/searchQueryHelper');
 
 app.use((req, res, next) => {
   res.header(
