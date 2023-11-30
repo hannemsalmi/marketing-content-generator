4207b5033464cfd41579badca81fe3fcc9b632ae
Hanne Salmi <hanne.salmi@metropolia.fi>
2023-11-30 10:59:10 +0200

    changes to formatting of text

--- messageBuilder/userMessageBuilder.js
+++ messageBuilder/userMessageBuilder.js
@@ -2,13 +2,13 @@ function buildMessage(topic, content_type, recipient, industry) { // Builds mess
     let finalContent_type;
 
     if (content_type == "instagram") {
-        finalContent_type = "bullet point list of 3 instagram caption suggestions";
+        finalContent_type = "bullet point list of 3 instagram caption suggestions. Please separate each point in list with line breaks as <br>.";
 
     } else if (content_type == "linkedin") {
-        finalContent_type = "linkedin post aimed at the customers of the company working in the specified role and industry. Please insert line breaks as <br>.";
+        finalContent_type = "linkedin post aimed at the customers of the company working in the specified role and industry. Please insert line breaks as <br> and insert two line breaks when the paragraph changes.";
 
     } else if (content_type == "email") {
-        finalContent_type = "email to send to the customer's mailing list. Please insert line breaks as <br> and bold the subject line with <b> and </b>.";
+        finalContent_type = "email to send to the customer's mailing list. Please insert line breaks as <br> and bold the subject line with <b> and </b>. Please also insert two line breaks after the subject and also when the paragraph changes.";
 
     }
 
