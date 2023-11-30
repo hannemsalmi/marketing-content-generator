489ea0a165613b629b4292bee47a48ffcb71c2cc
Hanne Salmi <hanne.salmi@metropolia.fi>
2023-11-30 10:46:21 +0200

    changes to instructions

--- messageBuilder/userMessageBuilder.js
+++ messageBuilder/userMessageBuilder.js
@@ -1,17 +1,21 @@
-function buildMessage (topic, content_type, recipient, industry) { // Builds message to be sent to GPT from the user's chosen preferences.
+function buildMessage(topic, content_type, recipient, industry) { // Builds message to be sent to GPT from the user's chosen preferences.
     let finalContent_type;
 
     if (content_type == "instagram") {
-        finalContent_type = "instagram caption";
+        finalContent_type = "bullet point list of 3 instagram caption suggestions";
 
-    } else {
-        finalContent_type = content_type;
+    } else if (content_type == "linkedin") {
+        finalContent_type = "linkedin post aimed at the customers of the company working in the specified role and industry. Please insert line breaks as <br>.";
+
+    } else if (content_type == "email") {
+        finalContent_type = "email to send to the customer's mailing list. Please insert line breaks as <br> and bold the subject line with <b> and </b>.";
 
     }
+
     return "Write a " + finalContent_type
-    + ". Topic: " + topic
-    + ". Recipient works in " + recipient
-    + " in " + industry + " industry.";
+        + ". Topic: " + topic
+        + ". Recipient works in " + recipient
+        + " in " + industry + " industry.";
 
 }
 
