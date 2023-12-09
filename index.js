4708d49fabac123c3dde688f2b76815c7370e048
Hanne Salmi <hanne.salmi@metropolia.fi>
2023-12-09 15:20:05 +0200

    changes to cors

--- index.js
+++ index.js
@@ -17,7 +17,7 @@ let systemMessage = process.env.DEFAULT_SYSTEM_MESSAGE;
 );*/
 
 
-app.use((req, res, next) => {
+/* app.use((req, res, next) => {
   res.header(
     "Access-Control-Allow-Origin",
     "https://aigen-dev-ed.develop.lightning.force.com"
@@ -28,7 +28,20 @@ app.use((req, res, next) => {
   );
   res.header("Access-Control-Allow-Methods", "GET, POST, OPTIONS");
   next();
-});
+}); */
+
+const corsOptions = {
+  origin: [
+    'https://aigen-dev-ed.develop.lightning.force.com',
+    'https://marketing-content-generator-02c05e08f82e.herokuapp.com'
+  ],
+  methods: 'GET, POST, OPTIONS',
+  allowedHeaders: 'Content-Type, Accept, Authorization',
+  credentials: true,
+};
+
+app.use(cors(corsOptions));
+
 
 app.use(express.json());
 
