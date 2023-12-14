96419fccf9d156532ede78c8d482606f7394ea4d
Fatlumi Metropolia <fatlum.gerguri@metropolia.fi>
2023-12-14 09:52:45 +0200

    Adding new Conent type

--- messageBuilder/userMessageBuilder.js
+++ messageBuilder/userMessageBuilder.js
@@ -1,37 +1,46 @@
 let instructions = {
-    instagram: process.env.INSTAGRAM_INSTRUCTION,
-    linkedin: process.env.LINKEDIN_INSTRUCTION,
-    email: process.env.EMAIL_INSTRUCTION
+  instagram: process.env.INSTAGRAM_INSTRUCTION,
+  linkedin: process.env.LINKEDIN_INSTRUCTION,
+  email: process.env.EMAIL_INSTRUCTION,
 };
 
-function buildMessage(topic, content_type, recipient, industry) { // Builds message to be sent to GPT from the user's chosen preferences.
-    let finalContent_type = instructions[content_type] || "default instructions";
+function buildMessage(topic, content_type, recipient, industry) {
+  // Builds message to be sent to GPT from the user's chosen preferences.
+  let finalContent_type = instructions[content_type] || "default instructions";
 
-    if (content_type == "instagram") {
-        finalContent_type = instructions['instagram'];
+  if (content_type == "instagram") {
+    finalContent_type = instructions["instagram"];
+  } else if (content_type == "linkedin") {
+    finalContent_type = instructions["linkedin"];
+  } else if (content_type == "email") {
+    finalContent_type = instructions["email"];
+  } else if (content_type == "whitepaper") {
+    finalContent_type = instructions["whitepaper"];
+  } else if (content_type == "blog") {
+    finalContent_type = instructions["blog"];
+  } else if (content_type == "e-book") {
+    finalContent_type = instructions["e-book"];
+  }
 
-    } else if (content_type == "linkedin") {
-        finalContent_type = instructions['linkedin'];
-
-    } else if (content_type == "email") {
-        finalContent_type = instructions['email'];
-
-    } 
-
-    return "Write " + finalContent_type
-        + ". Topic: " + topic
-        + ". Recipient works in " + recipient
-        + " in " + industry + " industry.";
-
+  return (
+    "Write " +
+    finalContent_type +
+    ". Topic: " +
+    topic +
+    ". Recipient works in " +
+    recipient +
+    " in " +
+    industry +
+    " industry."
+  );
 }
 
 function updateInstructions(newInstructions) {
-    instructions = { ...instructions, ...newInstructions };
+  instructions = { ...instructions, ...newInstructions };
 }
 
-
 module.exports = {
-    buildMessage,
-    updateInstructions,
-    getInstructions: () => instructions
+  buildMessage,
+  updateInstructions,
+  getInstructions: () => instructions,
 };
